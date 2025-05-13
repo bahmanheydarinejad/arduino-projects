910de9373a71643978ba59b60debc9e87cc1b311
Bahman Heydari Nejad <bahmanheydarinejad@gmail.com>
2025-05-14 00:42:35 +0330

    solid based SimpleJoyStickPad

--- Arduino/JoystickShield/JoystickShield.ino
+++ Arduino/JoystickShield/JoystickShield.ino
@@ -1,208 +1,155 @@
 #include <Arduino.h>
 
 const int joystickPrecision = 10;
-
 const int floorBound = 512 + joystickPrecision;
 const int surfaceBound = 512 - joystickPrecision;
 
-
-// تعریف پین‌های دکمه‌ها و جوی‌استیک
-const int A = 2;
-const int B = 3;
-const int C = 4;
-const int D = 5;
-const int E = 6;
-const int F = 7;
-const int SW = 8;
-const int UNKNOWN = 9;
-const int JOY_X = A0;  // محور X جوی‌استیک
-const int JOY_Y = A1;  // محور Y جوی‌استیک
-
-// کلاس برای جوی‌استیک
-class Joystick {
-private:
-  int xPin, yPin;
-  const char* name;
-  int lastX, lastY;
-
-public:
-  Joystick(int x, int y, const char* n)
-    : xPin(x), yPin(y), name(n), lastX(0), lastY(0) {
-    pinMode(xPin, INPUT);
-    pinMode(yPin, INPUT);
-  }
-
-  void checkPosition() {
-    int xValue = analogRead(xPin);  // صفر کردن چند بیت آخر
-    int yValue = analogRead(yPin);  // صفر کردن چند بیت آخر
-
-    bool isMoved = joystickMoved(xValue, yValue);
-
-    if (isMoved) {
-      Serial.print(name);
-      Serial.print(" - X: ");
-      Serial.print(xValue);
-      Serial.print(", Y: ");
-      Serial.println(yValue);
-    }
-
-    // lastX = xValue;
-    // lastY = yValue;
-  }
-
-  bool joystickMoved(int xValue, int yValue) {
-    bool isXMoved = xValue < surfaceBound || xValue > floorBound;
-    bool isYMoved = yValue < surfaceBound || yValue > floorBound;
-    return isXMoved || isYMoved;
-  }
+// Data structure to store states
+struct SimpleJoyStickPadData {
+  int A, B, C, D, E, F, SW, UNKNOWN;
+  int X, Y;
 };
 
-// کلاس پایه برای دکمه‌ها
+enum ButtonState {
+  UNHOLD = 0,
+  HOLD = 1,
+  PRESSED = 2,
+  RELEASED = 3
+};
+
+// Abstract base for buttons
 class Button {
 protected:
   int pin;
-  const char* name;
   bool lastState;
+  int* refValue;
 
 public:
-  Button(int p, const char* n)
-    : pin(p), name(n), lastState(HIGH) {
+  Button(int pin, int* ref) : pin(pin), lastState(HIGH), refValue(ref) {
     pinMode(pin, INPUT_PULLUP);
+    *refValue = UNHOLD;
   }
 
-  virtual bool isPressed(bool state) {
-    return lastState == HIGH && state == LOW;
-  }
+  void update() {
+    bool state = digitalRead(pin);
 
-  virtual bool isReleased(bool state) {
-    return lastState == LOW && state == HIGH;
-  }
-
-  virtual bool isHold(bool state) {
-    return lastState == LOW && state == LOW;
-  }
-
-  const char* getName() {
-    return name;
-  }
-
-  virtual void presseAction() {
-    Serial.print(name);
-    Serial.println(" button Pressed");
-  }
-
-  virtual void releaseAction() {
-    Serial.print(name);
-    Serial.println(" button Released");
-  }
-
-  virtual void holdAction() {
-    Serial.print(name);
-    Serial.println(" button Hold");
-  }
-
-  void checkAndAct() {
-    bool currentState = digitalRead(pin);
-    if (isPressed(currentState)) {
-      presseAction();
+    if (lastState == HIGH && state == LOW) {
+      *refValue = PRESSED;
+    } else if (lastState == LOW && state == HIGH) {
+      *refValue = RELEASED;
+    } else if (lastState == LOW && state == LOW) {
+      *refValue = HOLD;
+    } else {
+      *refValue = UNHOLD;
     }
-    if (isReleased(currentState)) {
-      releaseAction();
-    }
-    if (isHold(currentState)) {
-      holdAction();
-    }
-    lastState = currentState;
+
+    lastState = state;
   }
 };
 
-// کلاس برای دکمه‌ها
-class AButton : public Button {
-public:
-  AButton()
-    : Button(A, "A") {}
-};
-
-class BButton : public Button {
-public:
-  BButton()
-    : Button(B, "B") {}
-};
-
-class CButton : public Button {
-public:
-  CButton()
-    : Button(C, "C") {}
-};
-
-class DButton : public Button {
-public:
-  DButton()
-    : Button(D, "D") {}
-};
-
-class EButton : public Button {
-public:
-  EButton()
-    : Button(E, "E") {}
-};
-
-class FButton : public Button {
-public:
-  FButton()
-    : Button(F, "F") {}
-};
-
-class SWButton : public Button {
-public:
-  SWButton()
-    : Button(SW, "SW") {}
-};
-
-class UnknownButton : public Button {
-public:
-  UnknownButton()
-    : Button(UNKNOWN, "UNKNOWN") {}
-};
-
-// مدیریت دکمه‌ها و جوی‌استیک
-class ButtonManager {
+// Joystick class
+class Joystick {
 private:
-  Joystick* joystick;
-  Button* buttons[8];
+  int xPin, yPin;
+  int* xRef;
+  int* yRef;
 
 public:
-  ButtonManager() {
-    joystick = new Joystick(JOY_X, JOY_Y, "Joystick");
-    buttons[0] = new AButton();
-    buttons[1] = new BButton();
-    buttons[2] = new CButton();
-    buttons[3] = new DButton();
-    buttons[4] = new EButton();
-    buttons[5] = new FButton();
-    buttons[6] = new SWButton();
-    buttons[7] = new UnknownButton();
+  Joystick(int x, int y, int* xPtr, int* yPtr) : xPin(x), yPin(y), xRef(xPtr), yRef(yPtr) {
+    pinMode(xPin, INPUT);
+    pinMode(yPin, INPUT);
+    *xRef = 0;
+    *yRef = 0;
   }
 
-  void checkJoystick() {
-    joystick->checkPosition();
-  }
-
-  void checkButtons() {
-    for (int i = 0; i < 8; i++) {
-      buttons[i]->checkAndAct();
-    }
+  void update() {
+    int rawX = analogRead(xPin);
+    int rawY = analogRead(yPin);
+    *xRef = map(rawX, 0, 1023, -1024, 1024);
+    *yRef = map(rawY, 0, 1023, -1024, 1024);
   }
 };
 
-ButtonManager buttonManager;
+// Main Pad Controller
+class SimpleJoyStickPad {
+private:
+  Button* buttons[8];
+  Joystick* joystick;
+  SimpleJoyStickPadData data;
+
+public:
+  SimpleJoyStickPad(
+    int btnA, int btnB, int btnC, int btnD,
+    int btnE, int btnF, int btnSW, int btnUnknown,
+    int joyX, int joyY
+  ) {
+    buttons[0] = new Button(btnA, &data.A);
+    buttons[1] = new Button(btnB, &data.B);
+    buttons[2] = new Button(btnC, &data.C);
+    buttons[3] = new Button(btnD, &data.D);
+    buttons[4] = new Button(btnE, &data.E);
+    buttons[5] = new Button(btnF, &data.F);
+    buttons[6] = new Button(btnSW, &data.SW);
+    buttons[7] = new Button(btnUnknown, &data.UNKNOWN);
+
+    joystick = new Joystick(joyX, joyY, &data.X, &data.Y);
+  }
+
+  void action() {
+    for (int i = 0; i < 8; ++i) {
+      buttons[i]->update();
+    }
+    joystick->update();
+  }
+
+  SimpleJoyStickPadData getSimpleJoyStickPadData() {
+    action();
+    return data;
+  }
+};
+
+// Define input pins
+const int BTN_A = 2;
+const int BTN_B = 3;
+const int BTN_C = 4;
+const int BTN_D = 5;
+const int BTN_E = 6;
+const int BTN_F = 7;
+const int BTN_SW = 8;
+const int BTN_UNKNOWN = 9;
+const int JOY_X = A0;
+const int JOY_Y = A1;
+
+SimpleJoyStickPad pad(BTN_A, BTN_B, BTN_C, BTN_D, BTN_E, BTN_F, BTN_SW, BTN_UNKNOWN, JOY_X, JOY_Y);
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  buttonManager.checkButtons();
-  buttonManager.checkJoystick();
-  delay(10);  // برای کاهش نویز
+  SimpleJoyStickPadData data = pad.getSimpleJoyStickPadData();
+
+  Serial.print("Buttons: A=");
+  Serial.print(data.A);
+  Serial.print(" B=");
+  Serial.print(data.B);
+  Serial.print(" C=");
+  Serial.print(data.C);
+  Serial.print(" D=");
+  Serial.print(data.D);
+  Serial.print(" E=");
+  Serial.print(data.E);
+  Serial.print(" F=");
+  Serial.print(data.F);
+  Serial.print(" SW=");
+  Serial.print(data.SW);
+  Serial.print(" UNKNOWN=");
+  Serial.print(data.UNKNOWN);
+
+  Serial.print(" | Joystick: X=");
+  Serial.print(data.X);
+  Serial.print(" Y=");
+  Serial.println(data.Y);
+
+  delay(100);
 }
